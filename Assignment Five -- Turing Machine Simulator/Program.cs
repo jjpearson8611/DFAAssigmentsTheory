158583c759351d898c54294f541e6c81807e7f6a
Jason Pearson <jjpearson8611@gmail.com>
2014-11-12 16:51:44 -0500

    Added Next Assignment Skeleton
    Started working on my turing machine simulator and added folders for dfa
    and turing machine simulators

---
+++ Assignment Five -- Turing Machine Simulator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuringMachineSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first we ask the user what turing machine they would like to use
+            bool correctInput = false;
+
+            while (!correctInput)
+            {
+                Console.WriteLine("Which machine would you like to use?");
+                string input = Console.ReadLine();
+                int choice;
+
+                //make sure the user puts in an actual number
+                if (int.TryParse(input, out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Coming Soon");
+                            correctInput = true;
+                            break;
+
+                        case 2:
+                            Console.WriteLine("coming Soon");
+                            correctInput = true;
+                            break;
+                        default:
+                            Console.WriteLine("Please Insert a number choice");
+                            break;
+                    }
+                }
+            }
+
+            //we load that turing machine in
+
+
+            //we determine what the language is is for the input alphabet and show it to the user
+            // and ask them to create a string based on the input alphabet
+
+
+        }
+    }
+}
---
+++ Assignment Five -- Turing Machine Simulator/TuringMachine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace TuringMachineSimulator
+{
+    class TuringMachine
+    {
+
+        //constructors
+        public TuringMachine()
+        {
+            //Default Constructor
+        }
+
+        public TuringMachine(string fileLocation)
+        {
+
+        }
+
+        //properties
+        public int NumberOfStates
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Set of States this will also take care of the transition functions
+        /// </summary>
+        public List<State> States
+        {
+            get;
+            set; 
+        }
+
+        /// <summary>
+        /// Blank symbol
+        /// </summary>
+        public string Blank
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// tape alphabet symbols
+        /// </summary>
+        public List<string> Alphabet
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Set of input symbols
+        /// </summary>
+        public List<string> Input
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Final or accepting states
+        /// </summary>
+        public List<State> AcceptingStates
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// q0
+        /// </summary>
+        public State StartingState
+        {
+            get;
+            set;
+        }
+    }
+}
